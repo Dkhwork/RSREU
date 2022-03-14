6d77b0287b63a7c56c1966b32a480a3b83278024
dihk.work <dihkwork@gmail.com>
2022-03-14 12:04:41 +0300

    Version_2

---
+++ New.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#import pandas as pd
+from scipy import stats as sc
+#import statistics as st
+from scipy.stats import binom
+#import seaborn as sb
+
+s = np.random.normal(0.5, 0.1, 100)
+print(s)
+
+X_i = np.array(s)
+#X_ii = pd.Series(s)
